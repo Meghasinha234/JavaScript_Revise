0375f6d2b2b28577b454262a8abffeae9cee064d
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2023-12-16 19:26:57 +0000

    Variables

---
+++ Javascript_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=14455;
+let email="meghasinha761@gmail.com"
+var accountPassword="1234"
+accountCity="Ubud(Bali)"
+
+//  accountId=2345; //Not allowed
+email="megs@gmail.com"
+accountCity="Amed"
+accountPassword="1236"
+console.log(accountId);
+console.table([accountId,accountPassword,email,accountCity])
+// we should avoid using var because of block and functional scope issue
