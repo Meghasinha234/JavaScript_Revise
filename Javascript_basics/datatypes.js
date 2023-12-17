8ba7100c2c2051824d6dd4858fc16787a93ba965
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2023-12-17 10:05:13 +0000

    Datatypes

---
+++ Javascript_basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict" //treat code as a newer version of JS
+// alert(3+3) //we are using node Js not browser
+/*Datatypes
+1.Number +> 2 to power 53
+2.bigint
+3.string=""
+4.n=boolean+>true/false
+//null+>standalone value
+undefined
+symbol=>unique  */
+let name="megha"
+console.log(typeof(name));
+//Null is object 
