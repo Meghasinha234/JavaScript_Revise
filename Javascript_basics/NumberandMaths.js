21f86aba73c41afa0ce5a619792b3faf1e621f81
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2023-12-21 10:00:00 +0000

    Numbers and maths

---
+++ Javascript_basics/NumberandMaths.js
@@ -0,0 +1,16 @@
+const num=100.88;
+const num1=new Number(100);
+console.log(num);
+console.log(num1);
+console.log(num1.toString().length);
+console.log(num1.toFixed(2));
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+const num3=1000000;
+console.log(num3.toLocaleString('en-IN'));
+//***********Maths */
+console.log(Math.abs(-8));
+console.log(Math.round(-8.7239));
+console.log(Math.random());//value between 0-1
+console.log(Math.random()*10);
+//multiple by (max-min)+1 and add min  for getting value in range
