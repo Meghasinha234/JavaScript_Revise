9a9c4361acfcc70595c177d74de5a65fdc78d703
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2024-01-01 13:27:33 +0000

    Date and time

---
+++ Javascript_basics/DateAndTime.js
@@ -0,0 +1,14 @@
+let mydate=new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+let myDate= new Date(2023,1,15);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+let date= new Date("2023-01-27");
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+let timestamp=  Date.now();
+console.log(timestamp);
+console.log(date.getTime());
