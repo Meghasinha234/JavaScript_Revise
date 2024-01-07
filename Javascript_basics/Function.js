4a96c246691c32ba680a2976eb33da9aeeb7c2da
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2024-01-07 17:23:55 +0000

    function with object

--- Javascript_basics/Function.js
+++ Javascript_basics/Function.js
@@ -16,3 +16,15 @@ function login(username){
 }
 console.log(login("megha"));
 //if u will not pass any argument it will take it as undefined , u can use if/else for checking
+function circum(val1,...num){
+    return num;//will return array of all 100,200
+}
+console.log(circum(4,100,200));
+const user1={
+    name:"Megha",
+    Age:123
+}
+function object(anyObject){
+console.log(`user name is ${anyObject.name} and Age is ${anyObject.Age}`);
+}
+object(user1)
