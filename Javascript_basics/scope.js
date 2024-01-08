abe3f2417039710ef510089bc037efb6c12d54a5
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2024-01-08 08:54:26 +0000

    hoisting

--- Javascript_basics/scope.js
+++ Javascript_basics/scope.js
@@ -4,3 +4,12 @@ if(true){
     var c=30;//accessible even outside this 
 }
 console.log(c);//that's why we avoid using var
+//hoisting
+console.log(addnum(5,6));//you will be able to access
+function addnum(num1,num2){
+    return num1+num2;
+}
+console.log(subnum(5,4))//you can not access
+const sub=function subnum(num1,num2){
+    return num1-num2;
+}
