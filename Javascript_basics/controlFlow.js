7a1167cf2b47c7b8cfad436f8740a0795b7a1068
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2024-01-09 11:04:06 +0000

    controlflow

---
+++ Javascript_basics/controlFlow.js
@@ -0,0 +1,23 @@
+if(2=="2"){
+  console.log("equal");  
+}
+else if(2==="2"){
+console.log("not equal");
+}
+const month=2;
+  switch(month){
+case 1:
+  console.log("jan");
+  break;
+  case 2:
+    console.log("feb");
+    break;
+    default:
+      console.log("enter month properly");
+      break;
+  }
+  let val;
+  val=5??10;
+  console.log(val);//5
+  let val1=null??10;
+  console.log(val1);//10
