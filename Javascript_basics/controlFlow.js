2ad002464643801fe5653ff26c9afe8d65a5a863
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2024-01-13 06:12:06 +0000

    loops

--- Javascript_basics/controlFlow.js
+++ Javascript_basics/controlFlow.js
@@ -21,3 +21,22 @@ case 1:
   console.log(val);//5
   let val1=null??10;
   console.log(val1);//10
+//for loop
+const array=[1,2,4,"megs"];
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(element);
+  
+}
+//while loop
+let i=0;
+while (i<=2) {
+  console.log(i);
+  i++;
+}
+//do while loop
+let index=10;
+do {
+  console.log(index);
+  index++;
+} while (index<10);
